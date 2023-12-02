fe321d6aa4b1b2c28aee868a07867c0369fb20b4
Emre Eraslan <eraslan1494@gmail.com>
2023-12-02 16:11:57 +0300

    User Login Eklendi.

--- Emreraslan.Web/Controllers/UserController.cs
+++ Emreraslan.Web/Controllers/UserController.cs
@@ -26,5 +26,17 @@ namespace Emreraslan.Web.Controllers
             return Json(new { isOk = result.Item2, message = result.Item1 });
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Login(UserLoginDto user)
+        {
+            var result = await _userService.SignInUser(user);
+            return Json(new {isOk = result.Item2, message = result.Item1 });
+        }
     }
 }
