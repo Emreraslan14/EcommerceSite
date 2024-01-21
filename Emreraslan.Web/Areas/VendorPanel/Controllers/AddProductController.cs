bbfc9104aecca1a4e239288cf10498ff2588b65b
cankutay ucar <cankutayucar@hotmail.com>
2024-01-22 00:07:10 +0300

    .

--- Emreraslan.Web/Areas/VendorPanel/Controllers/AddProductController.cs
+++ Emreraslan.Web/Areas/VendorPanel/Controllers/AddProductController.cs
@@ -1,8 +1,10 @@
 ﻿using Emreraslan.Core.Entities;
 using Emreraslan.Services.Abstract;
 using Emreraslan.Web.Helpers;
+using Emreraslan.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace Emreraslan.Web.Areas.VendorPanel.Controllers
 {
@@ -33,64 +35,80 @@ namespace Emreraslan.Web.Areas.VendorPanel.Controllers
         }
 
 
+        //[HttpPost]
+        //public async Task<JsonResult> Index(Product prod)
+        //{
+        //    if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
+        //    {
+        //        var userName = _contextAccessor.HttpContext.User.Identity.Name;
+
+        //        var user = await _userManager.FindByNameAsync(userName);
+
+        //        if (user != null)
+        //        {
+        //            var vendor = _vendorService.Get(x => x.UserId == user.Id);
+        //            var category = _categoryService.Get(x => x.Name == vendor.Category);
+        //            prod.VendorId = vendor.Id;
+        //            prod.CategoryId = category.Id;
+
+        //            var result = await Task.FromResult(_productService.Insert(prod));
+
+        //            if (result == 1)
+        //                return Json(new { isOk = true, message = "Ürün kaydı başarıyla gerçekleşti." });
+
+        //            return Json(new { isOk = false, message = "Ürünü veri tabanına eklerken bir hatayla karşılaşıldı." });
+        //        }
+
+        //        return Json(new { isOk = false, message = "Kayıtlı kullanıcı bulunamadı." });
+        //    }
+
+        //    return Json(new { isOk = false, message = "Lütfen giriş yapınız." });
+        //}
+
         [HttpPost]
-        public async Task<JsonResult> Index(Product prod)
+        public async Task<IActionResult> UploadPhoto(ProductDto dto)
         {
-            if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
-            {
-                var userName = _contextAccessor.HttpContext.User.Identity.Name;
 
-                var user = await _userManager.FindByNameAsync(userName);
-                
-                if (user != null)
+
+            var randomFileName = ValueGenerater.FileNameGenerater(Path.GetExtension(dto.ProductPhoto.FileName));
+            string uploadPath = Path.Combine(_webHostEnvironment.ContentRootPath, "wwwroot", "VendorPanelContent", "images", "products", randomFileName);
+            using (FileStream stream = new FileStream(uploadPath, FileMode.Create))
+            {
+                await dto.ProductPhoto.CopyToAsync(stream);
+            }
+            var userName = _contextAccessor.HttpContext.User.Identity.Name;
+            var user = await _userManager.FindByNameAsync(userName);
+            if (user != null)
+            {
+                var vendor = _vendorService.Get(x => x.UserId == user.Id);
+                var category = _categoryService.Get(x => x.Name == vendor.Category);
+                var vendorid = vendor.Id;
+                var categoryid = category.Id;
+                _productService.Insert(new Product
                 {
-                    var vendor = _vendorService.Get(x => x.UserId == user.Id);
-                    var category = _categoryService.Get(x => x.Name == vendor.Category);
-                    prod.VendorId = vendor.Id;
-                    prod.CategoryId = category.Id;
+                    Brand = dto.Brand,
+                    Colour = dto.Colour,
+                    ChangedDate = DateTime.Now,
+                    Description = dto.Description,
+                    IsActive = true,
+                    IsDeleted = false,
+                    PhotoPath = uploadPath,
+                    Price = dto.Price,
+                    Stock = dto.Stock,
+                    Discount = dto.Discount,
+                    Name = dto.Name,
+                    CategoryId = categoryid,
+                    VendorId = vendorid,
 
-                    var result = await Task.FromResult(_productService.Insert(prod));
-
-                    if(result == 1)
-                        return Json( new {isOk = true , message = "Ürün kaydı başarıyla gerçekleşti."});
-
-                    return Json(new { isOk = false, message = "Ürünü veri tabanına eklerken bir hatayla karşılaşıldı." });
-                }
-
-                return Json(new {isOk = false , message = "Kayıtlı kullanıcı bulunamadı."});
+                });
             }
 
-            return Json(new { isOk = false, message = "Lütfen giriş yapınız." });
-        }
 
-        [HttpPost]
-        public JsonResult UploadPhoto()
-        {
-            IFormFileCollection files = Request.Form.Files;
-            
-            if(files.Count > 0)
-            {
-                var fileName = files[0].FileName;
-                var contentType = files[0].ContentType;
 
-                if (!contentType.StartsWith("image/"))
-                    return Json(new { isOk = false, message = "Lütfen Resim Dosyası Seçiniz..." });
 
-                var randomFileName = ValueGenerater.FileNameGenerater(Path.GetExtension(fileName));
 
-                string uploadPath = Path.Combine(_webHostEnvironment.ContentRootPath, "wwwroot/VendorPanelContent/images/products" , randomFileName);
 
-                using(var stream = new FileStream(uploadPath, FileMode.Create))
-                {
-                    files[0].CopyTo(stream);
-                }
-
-                return Json(new {isOk = true , path = "/VendorPanelContent/images/products/" + randomFileName });
-            }
-            else
-            {
-                return Json(new {isOk = false , message = "Lütfen Resim Dosyası Seçiniz..."});
-            }
+            return RedirectToAction("Index", "AddProduct", new { area = "VendorPanel" });
         }
     }
 }
